567629ef004167634d43440041f4a32f3347b47a
jasninus <jasninus8@hotmail.dk>
2020-01-31 20:32:20 +0100

    Add TimeScaled
    Also TimeScaledRigidbody WIP

---
+++ ProjectTimeLord/Assets/Scripts/TimeScaled.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimeScaled : MonoBehaviour
+{
+    [SerializeField] private float timeScale;
+
+    [SerializeField] private bool hasRigidbody;
+
+    public float TimeScale
+    {
+        get => timeScale;
+        set
+        {
+            timeScale = value;
+            UpdateTimeScale();
+        }
+    }
+
+    protected virtual void UpdateTimeScale()
+    {
+    }
+
+    protected virtual void Update()
+    {
+        TimeScaledUpdate(timeScale);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        TimeScaledFixedUpdate(timeScale);
+    }
+
+    public abstract void TimeScaledUpdate(float timeScale);
+
+    public abstract void TimeScaledFixedUpdate(float timeScale);
+}
---
+++ ProjectTimeLord/Assets/Scripts/TimeScaledRigidbody.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TimeScaledRigidbody : TimeScaled
+{
+    private Rigidbody2D rb;
+
+    private bool first;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void UpdateTimeScale()
+    {
+        if (!first)
+        {
+            rb.mass *= TimeScale;
+            rb.velocity /= TimeScale;
+            rb.angularVelocity /= TimeScale;
+        }
+        first = false;
+
+        //_timeScale = Mathf.Abs(value);
+
+        //rb.mass /= timeScale;
+        //rb.velocity *= timeScale;
+        //rb.angularVelocity *= timeScale;
+    }
+
+    public override void TimeScaledUpdate(float timeScale)
+    {
+    }
+
+    public override void TimeScaledFixedUpdate(float timeScale)
+    {
+    }
+}
