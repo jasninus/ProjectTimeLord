691002369f0a897f1f97e24aae489a178c234653
jasninus <jasninus8@hotmail.dk>
2020-01-31 23:57:46 +0100

    Add repairable objects
    With customizable rubble

---
+++ ProjectTimeLord/Assets/Scripts/Repairable.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RepairableEditor))]
+public class Repairable : MonoBehaviour
+{
+    [SerializeField] private float repairTime;
+
+    [SerializeField] private Transform repairedParent;
+
+    [SerializeField] private List<Behaviour> activateOnFinishRepair = new List<Behaviour>();
+
+    [SerializeField] private List<Rigidbody2D> rigidbodyActivateOnRepair = new List<Rigidbody2D>();
+
+    [SerializeField] private AnimationCurve repairProgressCurve;
+
+    private List<RepairableTransform> childTransforms = new List<RepairableTransform>();
+
+    private void Awake()
+    {
+        childTransforms = GetComponent<RepairableEditor>().GetChildFixedTransforms();
+        Debug.Log($"Awoken with {childTransforms.Count} children");
+
+        Invoke("Repair", 3);
+    }
+
+    private void Repair()
+    {
+        Debug.Log("Starting repair");
+        foreach (RepairableTransform trans in childTransforms)
+        {
+            trans.transform.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+
+        StartCoroutine(Repairing());
+    }
+
+    private IEnumerator Repairing()
+    {
+        List<RepairableTransform> brokenTransforms = SetupBrokenTransforms();
+
+        float startTime = Time.time, endTime = Time.time + repairTime;
+
+        while (endTime >= Time.time)
+        {
+            LerpChildren(brokenTransforms, startTime);
+            yield return null;
+        }
+
+        FinishRepair();
+    }
+
+    private List<RepairableTransform> SetupBrokenTransforms()
+    {
+        List<RepairableTransform> brokenTransforms = new List<RepairableTransform>();
+
+        foreach (RepairableTransform trans in childTransforms)
+        {
+            brokenTransforms.Add(new RepairableTransform(trans.transform.position, trans.transform.rotation));
+        }
+
+        return brokenTransforms;
+    }
+
+    private void LerpChildren(List<RepairableTransform> brokenTransforms, float startTime)
+    {
+        for (int i = 0; i < childTransforms.Count; i++)
+        {
+            childTransforms[i].transform.position = Vector3.Lerp(brokenTransforms[i].position, childTransforms[i].position,
+                repairProgressCurve.Evaluate((Time.time - startTime) / repairTime));
+            childTransforms[i].transform.rotation = Quaternion.Lerp(brokenTransforms[i].rotation,
+                childTransforms[i].rotation, repairProgressCurve.Evaluate((Time.time - startTime) / repairTime));
+        }
+    }
+
+    private void FinishRepair()
+    {
+        SnapPositionsDisableColliders();
+        EnableRequestedComponents();
+        EnableRequestedRigidbodies();
+        CheckAssignRepairParent();
+    }
+
+    private void SnapPositionsDisableColliders()
+    {
+        for (int i = 0; i < childTransforms.Count; i++)
+        {
+            childTransforms[i].transform.GetComponent<Collider2D>().enabled = false;
+            childTransforms[i].transform.position = childTransforms[i].position;
+        }
+    }
+
+    private void EnableRequestedComponents()
+    {
+        for (int i = 0; i < activateOnFinishRepair.Count; i++)
+        {
+            activateOnFinishRepair[i].enabled = true;
+        }
+    }
+
+    private void EnableRequestedRigidbodies()
+    {
+        for (int i = 0; i < rigidbodyActivateOnRepair.Count; i++)
+        {
+            rigidbodyActivateOnRepair[i].isKinematic = false;
+        }
+    }
+
+    private void CheckAssignRepairParent()
+    {
+        if (repairedParent)
+        {
+            transform.parent = repairedParent;
+        }
+    }
+}
