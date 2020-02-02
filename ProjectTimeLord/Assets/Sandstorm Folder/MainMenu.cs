264ad93c7f1d26509a6e5c44d565933c56caa774
jasninus <jasninus8@hotmail.dk>
2020-02-02 15:26:28 +0100

    Credits

--- ProjectTimeLord/Assets/Sandstorm Folder/MainMenu.cs
+++ ProjectTimeLord/Assets/Sandstorm Folder/MainMenu.cs
@@ -6,13 +6,23 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-   public void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(2);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
